3e3a0e9d51d7d4c1848667604aef4fc27048ac7b
argnulluser <44545510+argnulluser@users.noreply.github.com>
2025-07-25 11:31:09 +0300

    Initialization

---
+++ src/main/java/com/example/book_management_system_api/BookManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book_management_system_api/BookManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_management_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
