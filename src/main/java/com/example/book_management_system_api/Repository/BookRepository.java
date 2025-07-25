1bf4b45f5699aa537d82df6789d502921977d78d
argnulluser <44545510+argnulluser@users.noreply.github.com>
2025-07-25 12:02:00 +0300

    Add repository

---
+++ src/main/java/com/example/book_management_system_api/Repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.book_management_system_api.Repository;
+
+import com.example.book_management_system_api.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
